5096195d4b9f9d1c4fc8c10f4954c3f08cdacfef
wu241617 <1272689028@qq.com>
2021-02-12 11:19:36 +0800

    feat:2021/2/12

--- 41_express/index.js
+++ 41_express/index.js
@@ -1,6 +1,5 @@
 //  node.js 一个基于javascript运行环境的服务器语言，它的出现使得javascript有能力去实现服务器的操作，
 //  在GitHub上node.js的star数已经接近6万，可见其受欢迎程度；而基于node.js的Express则把原先的许多操作
-//  变得简单灵活，一系列强大特性帮助你创建各种Web应用，和丰富的HTTP工具，使用Express可以快速构建一个完整功能的网站
-// 1，安装方式 依次输入以下命令运行，npm init   npm install    npm install express --save
-// 全局安装 npm i express-generator -g ,通过express --version 查看版本
-// 通过express --view=e.js app 创建项目 （e.js为模板，可选）
+//  变得简单灵活，一系列强大特性帮助你创建各种 Web 应用，和丰富的HTTP工具，使用Express可以快速构建一个完整功能的网站
+// 1，安装方式 依次输入以下命令运行，npm init      npm install express --save
+// 2，全局安装 npm i express-generator -g ,通过 express --version 查看版本。通过express --view=e.js app 创建项目 （e.js为模板，可选），npm install 自动下载安装 package.json文件中记录的依赖版本
